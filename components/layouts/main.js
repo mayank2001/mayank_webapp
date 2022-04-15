29cf28648ac072552cb072c2dc6f037752f3aeb2
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2022-04-15 23:59:34 +0530

    last

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,7 +8,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Mayank Joshi's - Website</title>
+        <title>Mayank Joshi&apos;s - Website</title>
       </Head>
 
         <Navbar path= {router.asPath}/>
